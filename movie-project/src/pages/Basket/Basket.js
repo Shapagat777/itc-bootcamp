e384ae526185fd0c77dbd908825191cfedba1d60
shapagat77700 <shapagat77700@gmail.com>
2023-02-10 09:53:23 +0600

    Drilling and props

--- movie-project/src/pages/Basket/Basket.js
+++ movie-project/src/pages/Basket/Basket.js
@@ -1,14 +1,21 @@
 import { ModalContext } from "../../providers/ModalProvider";
 import { useContext } from "react";
+import Header from "../../components/Header/Header";
 
 const Basket = () => {
 
     // useContext (контекст, который вы использовали в провайдере) 
     // возвращает данные которые в провайдере
-    const {cart} = useContext(ModalContext)
+    const { cart } = useContext(ModalContext)
 
     return (
-        <div>{cart.map(el => <p>{el.title}</p>)}</div>
+        <div>{cart.map(el => <p>{el.title}</p>)}
+          <div className="background">
+              <div className="inner__list"> 
+              </div>
+           
+         </div>
+         </div>
     )
 }
 
